884b840e3567dc72b29c675ca216883486ed2ad6
dudenheryana1994 <heryana.duden@gmail.com>
2020-04-07 12:33:58 +0700

    Upadate Progress 7 April

--- app/src/main/java/com/dudenakb/pretest/MainActivity.java
+++ app/src/main/java/com/dudenakb/pretest/MainActivity.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+//Tanggal Pengerjaan : 7 april 2020
+//Deskripsi Pengerjaan : Mengerjakan Selanjutnya.java dan KelasAkhir.java
+//NIM : 10117137
+//Nama : Duden Heryana
+//Kelas : IF-4
 
 public class MainActivity extends AppCompatActivity {
 
