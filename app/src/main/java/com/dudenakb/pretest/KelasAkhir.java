731c90aa6be6b5e3f04fb311dbfc8afdf5799b63
dudenheryana1994 <heryana.duden@gmail.com>
2020-04-07 11:48:16 +0700

    Update Progress 7-April

--- app/src/main/java/com/dudenakb/pretest/KelasAkhir.java
+++ app/src/main/java/com/dudenakb/pretest/KelasAkhir.java
@@ -3,13 +3,28 @@ package com.dudenakb.pretest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class KelasAkhir extends AppCompatActivity {
+   TextView namasatu;
+    TextView namadua;
+    TextView namatiga;
+
+    private String textnama;
+   private String Names = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kelas_akhir);
+        namasatu = (TextView)findViewById(R.id.namasatu);
+        namadua = (TextView)findViewById(R.id.namadua);
+        namatiga = (TextView)findViewById(R.id.namatiga);
+        Bundle extras = getIntent().getExtras();
+        textnama = extras.getString(Names);
+        namasatu.setText("" + textnama + " ");
+        namadua.setText("" + textnama + " ");
+        namatiga.setText("" + textnama + " ");
 
 
     }
